d14fb60c1694bd038ecfa3a3da305f1fe0242d8c
Saaketh Medepalli <saakethm@umich.edu>
2025-01-29 17:17:23 -0500

    considerations for word generation:

--- app/services/word_service.py
+++ app/services/word_service.py
@@ -1,5 +1,15 @@
 from transformers import AutoModelForCausalLM, AutoTokenizer
 
+
+# TODO: way forward
+# 1. for myself: use ollama to generate words locally using model of choice (mistral-7b or llama3-8b) 
+# 2. for users: use gpt4o-mini to generate words using my API key (charged to me for now); precompute a list of words for several themes 
+# and shuffle them for each request to avoid generating the same words each time (if there's a hit, no need to query the API; otherwise,
+# query the API and save the result for future requests)
+
+# for either option, best would be to include a shuffle option to shuffle the existing words while generating a larger list so the user 
+# doesn't keep generating
+
 model_name = "gpt2"
 tokenizer = AutoTokenizer.from_pretrained(model_name)
 model = AutoModelForCausalLM.from_pretrained(model_name)
